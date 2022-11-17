0d3d8f2248e6773e1c262f3960ccd024ac2f952c
AbhayPandey-CS <abhay80010@gmail.com>
2022-11-17 22:43:53 +0530

    first commit

---
+++ src/dataSource/DataArray.java
@@ -0,0 +1,21 @@
+package dataSource;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class DataArray {
+
+	
+	public ArrayList<DataSource> array=null;
+	public DataArray(){
+		array=new ArrayList<DataSource>();
+	}
+	public synchronized void addData(DataSource dataSource){
+		array.add(dataSource);
+	}
+	public Iterator<DataSource> iterator(){
+		return array.iterator();
+	}
+}
+
